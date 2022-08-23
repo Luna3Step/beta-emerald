39033b79a09b262452744306f2535f182bb25325
Luna <lunagooch02@gmail.com>
2022-08-23 11:24:17 +0100

    Move Item in Party

--- include/strings.h
+++ include/strings.h
@@ -3082,4 +3082,12 @@ extern const u8 gText_ExpShareOff[];
 
 extern const u8 gText_ThrowInPremierBalls[];
 
+//Move Item
+
+extern const u8 gMenuText_Move[];
+extern const u8 gText_MoveItemWhere[];
+extern const u8 gText_XsYAnd[];
+extern const u8 gText_XsYWereSwapped[];
+
+
 #endif // GUARD_STRINGS_H
